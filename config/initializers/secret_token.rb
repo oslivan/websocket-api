d39950fcc414013f6e7d956e201654a4179bd559
zhangyanpei <oslivan@163.com>
2018-09-05 14:42:59 +1000

    init demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebsockeApi::Application.config.secret_key_base = '02df9b5480689eb9b9988ce934a96520ecf91d4f4608344f056269bef4eddcdbcbd8311acc3e1623ddc86542acdf091e06e0a8b1001fc81cfe0093840aa576e2'
